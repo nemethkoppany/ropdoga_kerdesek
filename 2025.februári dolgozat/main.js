e6149a8a4413bfbe6aac7e1c47ab1bcfe7365c55
nemethkoppany <koppany344@gmail.com>
2025-02-25 19:46:49 +0100

    9. feladat javítás

--- 2025.februári dolgozat/main.js
+++ 2025.februári dolgozat/main.js
@@ -1,14 +1,18 @@
-const fv = (a,b, c) => {
-   if(a.eletkor > b.eletkor){
-     console.log(a.name)
-   }
-}
-const persList = [{
-	eletkor: 16,
-	name: 'József'
-},
-{
-	eletkor: 18,
-	name: 'Ferenc'
-}]
-fv(persList[0], persList[1], undefined)
+const fv = (a,b) => {
+	console.log(a)
+	console.log(b)
+	console.log(a.eletkor)
+	console.log(b.eletkor)
+	if(a.eletkor > b.eletkor){
+	  console.log(a.name)
+	}
+  }
+  const persList = [{
+   eletkor: 16,
+   name: 'József'
+  },
+  {
+   eletkor: 16,
+   name: 'Ferenc'
+  }]
+  fv(persList, persList[1])
